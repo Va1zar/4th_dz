01686471604906589a6aaeaf85f77131f47d17d4
Evgeniy <s.h.a.r.k.112@mail.ru>
2022-12-11 20:32:59 +0700

    solve of 29

---
+++ 29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая выводит массив из 8 элементов, и выводит их на экран
+
+int[] array = new int[8];
+void FillArray(int[] coll)
+{
+    int Length = coll.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        coll[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+void PrintArray(int[] coll2)
+{
+    int count = coll2.Length;
+    int position = 0;
+    while (position < count)
+    {
+
+        Console.Write(coll2[position] + ", ");
+        position++;
+    }
+}
+Console.Write("[");
+FillArray(array);
+PrintArray(array);
+Console.Write("]");
